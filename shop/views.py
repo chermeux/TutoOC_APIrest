152fb3146bc9f0ca1ff201284f52eb4d3dc26444
Clém'INT <clement.chermeux@imt-bs.eu>
2023-07-11 17:03:18 +0200

    rajouter des filtres suite deja à l'utilisation de routers pour les urls

--- shop/views.py
+++ shop/views.py
@@ -1,20 +1,37 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
+
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+class CategoryViewset(ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        return Category.objects.all()
 
 
-class CategoryAPIView(APIView):
+class ProductViewset(ReadOnlyModelViewSet):
+    serializer_class = ProductSerializer
 
-    def get(self, *args, **kwargs):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        # Nous récupérons tous les produits dans une variable nommée queryset
+        queryset = Product.objects.filter(active=True)
+        # Vérifions la présence du paramètre ‘category_id’ dans l’url et si oui alors appliquons notre filtre
+        category_id = self.request.GET.get('category_id')
+        if category_id is not None:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
 
-class ProductAPIView(APIView):
+class ArticleViewset(ReadOnlyModelViewSet):
 
-    def get(self, *args, **kwargs):
-        Products = Product.objects.all()
-        serializer = ProductSerializer(Products, many=True)
-        return Response(serializer.data)
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        product_id = self.request.GET.get('product_id')
+        if product_id is not None:
+            queryset = queryset.filter(product_id=product_id)
+        return queryset
